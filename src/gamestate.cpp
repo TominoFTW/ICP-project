1d8b2a77dc6da12525659e84013d1a461bcb0c9c
Jakub <jakubkontrik@gmail.com>
2023-05-04 00:36:47 +0200

    Add key pick function

--- src/gamestate.cpp
+++ src/gamestate.cpp
@@ -5,6 +5,9 @@
 #include <QGraphicsRectItem>
 #include <iostream>
 #include "ghost.h"
+#include "pacman.h"
+#include "key.h"
+#include "map.h"
 
 
 GameState::GameState(Pacman *pacman, QGraphicsScene *scene, Map &map) : scene(scene),pacman(pacman), map(map){
@@ -15,15 +18,24 @@ GameState::GameState(Pacman *pacman, QGraphicsScene *scene, Map &map) : scene(sc
 void GameState::add_ghost(Ghost &ghost){
     this->ghosts.push_back(&ghost);
 }
+void GameState::add_key(Key &key){
+    this->keys.push_back(&key);
+}
 void GameState::set_pacman_dir(int direction){
     this->pacman->set_direction(direction);
 }
 void GameState::update(){
     this->pacman->move(0, *this->scene, this->map);
-    std::cout << "updateAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAaa" << std::endl;
     for (Ghost *ghost : this->ghosts){
         std::cout << "update ghost" << std::endl;
         ghost->move(0, *this->scene, this->map, *this->pacman);
+    }
+    // todo check collision moze byt aj tu ak to bude bugnute :D
+    for (Key *key : this->keys){
+        std::cout << "update ghaaaaaaaaaaaaaaaaaaaaaaostupdate ghost" << key->position.first <<" "<< this->pacman->position.first<< std::endl;
+        if (key->position == this->pacman->position){
+            key->pick();    
+        }
     }
     // update pacman
     // update ghosts
